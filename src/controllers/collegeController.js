a95b892184697bda2657596809eaea41e21192c6
bidyut10 <bidyut34268@gmail.com>
2022-06-30 10:38:28 +0530

    2postapiDone

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,56 +1,60 @@
-
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 const collegeModel = require("../model/collegeModel")
 //const internModel = require("../model/internModel")
 
-const createCollege = async function (req,res){
-try{
-    let data = req.body
+const createCollege = async function (req, res) {
+    try {
+        // Edge cases
+        let data = req.body
 
-    // Edge cases
-    //this is for if user or student provide given an empty body
-    if(Object.keys(data).length==0){
-        res .status(400).send({ status:false , msg:"Please provide the college details"})
-    }
-    //particular each required feild is mandetory
-    if(!req.body.name){
-        res.status(400).send({status:false , msg:"Name is required"})
-    }
-    if(!req.body.fullName){
-        res.status(400).send({status:false , msg:"FullName is required"})
-    }
-    if(!req.body.logoLink){
-        res.status(400).send({status:false , msg:"logoLink is required"})
-    }
-    //checking name and fullname in alphabet only
-    let regexName = /^\s*(?=[A-Z])[\w\.\s]{2,64}\s*$/
+        //this is for if user or student provide given an empty body
+        if (Object.keys(data).length == 0) {
+           return res.status(400).send({ status: false, msg: "Please provide the college details" })
+        };
+        //particular each required feild is mandetory
+        if (!data.name) {
+           return res.status(400).send({ status: false, msg: "Name is required" })
+        };
+        if (!data.fullName) {
+           return res.status(400).send({ status: false, msg: "FullName is required" })
+        };
+        if (!data.logoLink) {
+           return res.status(400).send({ status: false, msg: "logoLink is required" })
+        };
+        //checking name and fullname in alphabet only
 
-    if(!regexName.test(req.body.name)){
-        res.status(400).send({status:false , msg:"Name should be alphabat type"})
-    }
-    if(!regexName.test(req.body.fullName)){
-        res.status(400).send({status:false , msg:"fullname should be alphabat type"})
-    }
+        if (!(/^\s*([a-zA-Z\s\,]){2,64}\s*$/.test(data.name))) {
+           return res.status(400).send({ status: false, msg: "Name should be in alphabat type" })
+        };
+        if (!(/^\s*([a-zA-Z\s\,]){2,64}\s*$/.test(data.fullName))) {
+           return res.status(400).send({ status: false, msg: "fullname should be in alphabat type" })
+        };
 
-    //checking the name is unique or not
-    let nameCheck = await collegeModel.findOne({name:req.body.name})
-    if(!nameCheck){
-        let college = await collegeModel.create(data)
-        res.status(200).send({status:true, msg:"college create successfully",data:college})
+        //checking logo format
+        if (!(/^https?:\/\/(.+\/)+.+(\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif|jfif))$/i.test(data.logoLink))) {
+           return res.status(400).send({ status: false, msg: "Logolink is not in correct format" })
+        };
+
+        //checking college fullname is already registerd or not
+        let isValidfullName = await collegeModel.findOne({fullName:data.fullName});
+        if (isValidfullName) {
+            return res.status(400).send({ status: false, message: "College fullName is already registered", })
+        };
+
+        //checking the name is unique or not
+        let nameCheck = await collegeModel.findOne({ name: data.name });
+        if (!nameCheck) {
+            let college = await collegeModel.create(data)
+           return res.status(200).send({ status: true, msg: "college create successfully", data: college });
         }
-        else{
-            res.status(400).send({status:false , msg:"Name should be unique"})
+        else {
+           return res.status(400).send({ status: false, msg: "Name should be unique" });
         }
-} catch (err) {
-    console.log("This is the error :", err.message);
-    res.status(500).send({ msg: "Error", error: err.message });
-}     
+    } catch (err) {
+        console.log("This is the error :", err.message);
+       return res.status(500).send({ msg: "Error", error: err.message });
+    }
 
-}
-
+};
 
 module.exports.createCollege = createCollege
-
-
-
-
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,85 +1,78 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 const collegeModel = require("../model/collegeModel")
 const internModel = require("../model/internModel")
 
-const createIntern = async function (req,res){
-try{
-    let data = req.body
+const createIntern = async function (req, res) {
+    try {
+        let data = req.body
 
-     // Edge cases
-     //if user or student given a empty body
-     if(Object.keys(data).length==0){
-        res .status(400).send({ status:false , msg:"Please provide the college details"})
-    }
-    //each required feild is mandetory
-    if(!req.body.name){
-        res.status(400).send({status:false , msg:"Name is required"})
-    }
-    if(!req.body.email){
-        res.status(400).send({status:false , msg:"Email is required"})
-    }
-    if(!req.body.mobile){
-        res.status(400).send({status:false , msg:"Mobile is required"})
-    }
-    if(!req.body.collegeId){
-        res.status(400).send({status:false , msg:"collegeId is required"})
-    }
+        // Edge cases
+        //if user or student given a empty body
+        if (Object.keys(data).length == 0) {
+           return res.status(400).send({ status: false, msg: "Please provide the college details" })
+        };
+        //each required feild is mandetory
+        if (!data.name) {
+           return res.status(400).send({ status: false, msg: "Name is required" })
+        };
+        if (!data.email) {
+           return res.status(400).send({ status: false, msg: "Email is required" })
+        };
+        if (!data.mobile) {
+           return res.status(400).send({ status: false, msg: "Mobile is required" })
+        };
+        if (!data.collegeId) {
+           return res.status(400).send({ status: false, msg: "collegeId is required" })
+        };
 
-    // checking name in alphbet only
-    
-    let regexName = /^\s*(?=[A-Z])[\w\.\s]{2,64}\s*$/
-    
-    if(!regexName.test(req.body.name)){
-        res.status(400).send({status:false , msg:"Name should be alphabat type"})
-    }
+        // checking name in alphbet only
 
-    // checking email is in right format or not and also the email is unique or not
+        if (!(/^\s*([a-zA-Z\s\,]){2,64}\s*$/.test(data.name))) {
+           return res.status(400).send({ status: false, msg: "Name should be alphabat type" })
+        };
 
-     if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(req.body.email))) {
-        return res.status(400).send({ status: false, msg: "please Enter Valid Email" })
-      }
-  
-      const isEmailPresent = await internModel.findOne({ email: req.body.email })
-  
-      if (isEmailPresent) {
-        return res.status(400).send({ status: false, msg: "EmailId Is Already Exist In DB" })
-    }
+        // checking email is in right format or not and also the email is unique or not
+
+        if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(data.email))) {
+            return res.status(400).send({ status: false, msg: "please Enter Valid Email" })
+        };
+
+        const isEmailPresent = await internModel.findOne({ email: data.email })
+
+        if (isEmailPresent) {
+            return res.status(400).send({ status: false, msg: "EmailId Is Already Exist In DB" })
+        };
 
 
-     //checking mobile is in right format or not and also the mobile is present or not in our database
+        //checking mobile is in right format or not and also the mobile is present or not in our database
 
-    let regexMobile = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
-    
-    if(!regexMobile.test(req.body.mobile)){
+        if (!(/^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/.test(data.mobile))) {
+
+            return res.status(400).send({ status: false, msg: "Mobile is not valid" })
+        };
+
+        const isMobilePresent = await internModel.findOne({ mobile: data.mobile })
+
+        if (isMobilePresent) {
+            return res.status(400).send({ status: false, msg: "Mobile is already register" })
+        };
+        let collegeId=data.collegeId
+
+        let collegeCheck = await collegeModel.findById({ _id:data.collegeId })
         
-        return res.status(400).send({ status: false, msg: "Mobile is not valid" })
+        if (collegeCheck) {
+
+            let college = await internModel.create(data)
+            return res.status(200).send({ status: true, msg: "college create successfully", data: college })
+        }
+        else{
+            return res.status(400).send({ status: false, msg: "CollegeId was not found" })
+        };
+    } catch (err) {
+        console.log("This is the error :", err.message);
+       return res.status(500).send({ msg: "Error", error: err.message });
     }
-    
-    const isMobilePresent = await internModel.findOne({ mobile: req.body.mobile })
 
-    if (isMobilePresent) {
-      return res.status(400).send({ status: false, msg: "Mobile is already register" })
-  }
-
-
-
-  let collegeCheck = await internModel.findById({_id:req.body.collegeId}) //check
-
-  if(!collegeCheck){
-
-      let college = await internModel.create(data)
-      res.status(200).send({status:true, msg:"college create successfully",data:college})
-      }
-      else{
-          res.status(400).send({status:false , msg:"collegeId is already register"})
-      }
-} catch (err) {
-    console.log("This is the error :", err.message);
-    res.status(500).send({ msg: "Error", error: err.message });
-}
-
-}
+};
 
 module.exports.createIntern = createIntern
-
-//mobile: /^[0]?[6789]\d{9}$/
